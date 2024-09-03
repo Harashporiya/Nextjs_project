feb9f144436095215c79dfaf5365bdec0ca3fb52
Your Name <your_email@example.com>
2024-09-03 15:53:43 +0530

    getall delivery person endpoint

--- choco/src/app/api/delivery-person/route.ts
+++ choco/src/app/api/delivery-person/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPerson } from "@/lib/db/schema";
+import { deliveryPerson, wareHouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validators/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request:Request){
     const requestData = await request.json()
@@ -17,5 +18,22 @@ export async function POST(request:Request){
         return Response.json({message:"OK"},{status:200})
     } catch (error) {
         return Response.json({error,message:"Failed to store the delivery person into the dataBase"},{status:500})
+    }
+}
+
+export async function GET(){
+    try {
+        const alldeliverPerson = await db.select({
+          id:deliveryPerson.id,
+          name:deliveryPerson.name,
+          phone:deliveryPerson.phone,
+          wareHouses:wareHouses.name,
+        })
+        .from(deliveryPerson)
+        .leftJoin(wareHouses,eq(deliveryPerson.warehouseId, wareHouses.id))
+        .orderBy(desc(deliveryPerson.id))
+        return Response.json(alldeliverPerson)
+    } catch (error) {
+        return Response.json({message:"Failed to fetch delivery person"},{status:500})
     }
 }
