43aea068fbbb841d2a184b384e158ee191444b89
Your Name <your_email@example.com>
2024-09-05 21:55:52 +0530

    component set

--- choco/src/app/admin/warehouses/page.tsx
+++ choco/src/app/admin/warehouses/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Button } from '@/components/ui/button'
 import React from 'react'
-import {DataTable} from './_components/data-table'
+import {DataTable} from '../_components/data-table'
 import { columns } from './_components/columns'
 import { useQuery } from '@tanstack/react-query'
 import {  getAllWarehouses } from '@/http/api'
