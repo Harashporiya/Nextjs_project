62d1ce70f010e6d1cd3c0476787d92c2d8f14e80
Your Name <your_email@example.com>
2024-09-05 16:30:32 +0530

    add loading spinner for fetching products

--- choco/src/app/admin/products/page.tsx
+++ choco/src/app/admin/products/page.tsx
@@ -8,22 +8,34 @@ import { getAllProducts } from '@/http/api'
 import { Product } from '@/types'
 import ProductSheet from './product-sheet'
 import { useNewProduct } from '@/store/product/product-store'
+import { Loader2 } from 'lucide-react'
 
 const ProductsPage = () => {
     const {onOpen} = useNewProduct()
-    const {data:products} = useQuery<Product[]>({
+    const {data:products,isLoading} = useQuery<Product[]>({
         queryKey:['products'],
         queryFn:getAllProducts,
     })
 
   return (<>
+  <div>
     <div className='flex items-center justify-between'>
      <h3 className='text-2xl font-bold tracking-tight'>Products</h3>
      <Button size={'sm'}  onClick={onOpen}>Add Product</Button>
     
     </div>
     <ProductSheet/>
-    <DataTable columns={columns}  data={products || []}/>
+    </div>
+
+    {
+      isLoading ? ( <div className='flex items-center justify-center'>
+        <Loader2 className='size-10 animate-spin'/>
+      </div>
+    ): <DataTable columns={columns}  data={products || []}/>
+    }
+    
+    
+   
     </>
   )
 }
